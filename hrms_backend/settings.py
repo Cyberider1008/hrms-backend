f4366422f8f4c3348fe2be2cd64ae30beedd8b7c
cyberider1008 <abaranwal.it@gmail.com>
2026-03-18 00:20:43 +0530

    add runtime txt

--- hrms_backend/settings.py
+++ hrms_backend/settings.py
@@ -129,3 +129,16 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+LOGGING = {
+    "version": 1,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
